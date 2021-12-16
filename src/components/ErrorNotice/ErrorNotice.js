6e526ea9d5c1a9df274fe2c040cb665fa38377bc
BhavyaMehta022 <bhavyamehta022@gmail.com>
2021-12-16 22:28:27 +0530

    added error notice components

---
+++ src/components/ErrorNotice/ErrorNotice.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './ErrorNotice.module.css';
+import Button from '../../elements/Button/Button';
+
+const errorNotice = (props) => {
+    return(
+        <div className={classes.ErrorNoticeWrapper}>
+            <h1 className={classes.NotFound404}>404</h1>
+            <div className={classes.ErrorTextWrapper}>
+                <h2 className={classes.NotFoundHeading}>Oops!</h2>
+                <p className={classes.NotFoundDetails}>We can't find the city you are looking for.</p>
+            </div>
+        </div>
+    );
+}
+
+export default errorNotice;
